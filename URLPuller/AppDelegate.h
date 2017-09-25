65b6358fe0b08bf6787e8008d750f8019ad8d5f3
ganleyth <tganley3@gmail.com>
2017-09-25 12:24:03 -0400

    Initial commit

---
+++ URLPuller/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  URLPuller
+//
+//  Created by Thomas Ganley on 9/25/17.
+//  Copyright © 2017 Thomas Ganley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
